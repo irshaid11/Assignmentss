6d39af2c2021dc215d1971d747d568b168656c91
Samir <samir.irshaid@gmail.com>
2021-02-04 15:30:41 +0100

    Assignment 1

---
+++ Assigment 1/assignment.py
@@ -0,0 +1,29 @@
+1-Conver Decimal numbers to binary
+
+5
+31
+112
+255
+
+
+2-convert Binary numbers to Decimal
+
+11110001
+11001010
+11111111
+00011100
+
+
+
+3-Convert this text to binary
+
+-Hello 
+-Python
+
+
+4-convert this binary to text
+
+01011001 01101111 1110101
+01000100 01101001 1100100
+01000111 01101111 1101111 1100100
+1001010 01101111 1100010
